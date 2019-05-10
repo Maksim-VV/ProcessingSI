16558858f96013315995a83dd131f0838fba1c94
MaximVV <laul@bk.ru>
2019-05-11 00:02:29 +0400

    clean template

---
+++ src/main/java/com/vasiliska/ProcessingSI/ProcessingSiApplication.java
@@ -0,0 +1,13 @@
+package com.vasiliska.ProcessingSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingSiApplication.class, args);
+	}
+
+}
